fa8bae0ac9ad91521420cf20c187920ba9f405c2
khanhtrinhDSS <khanh.tt@dssolution.jp>
2024-12-13 11:26:28 +0700

    remove button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { getToken } from 'firebase/messaging';
 import { messaging } from './firebase';
@@ -8,19 +8,22 @@ function App() {
   const [text, setText] = useState();
   const params = new URLSearchParams(window.location.search);
 
-  const handleRequest = async () => {
-    const permission = await Notification.requestPermission();
-    console.log('permission: ', permission);
-      const token = await getToken(messaging, {
-        vapidKey: "BH6KL1kaSE6gVfIjv5LFK-rQsUdLuDwp7cF3bgKefw6oKFe0aF19igZxvl4b_N2F8afpqTghrgHYlQ_MgxJ-D7c"
-      })
-      setText(token);
-  }
+  useEffect(() => {
+    const fetchPermission = async () => {
+      const permission = await Notification.requestPermission();
+      console.log('permission: ', permission);
+        const token = await getToken(messaging, {
+          vapidKey: "BH6KL1kaSE6gVfIjv5LFK-rQsUdLuDwp7cF3bgKefw6oKFe0aF19igZxvl4b_N2F8afpqTghrgHYlQ_MgxJ-D7c"
+        })
+        setText(token);
+    }
+    fetchPermission();
+  }, [])
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={handleRequest} >Request</button>
+        {/* <button onClick={handleRequest} >Request</button> */}
         <div className="text-container">
         <p style={{padding: "12px"}}>{text}</p>
         </div>
